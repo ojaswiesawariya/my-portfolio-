0f7e4f63c7701172cde22c4b791ccde4166092b5
ojaswiesawariya <ojaswisawariya00@gmail.com>
2026-09-07 17:46:22 +0530

    Update Customisable_AI_Chatbot_with_memory_feature.py

--- Customisable_AI_Chatbot_with_memory_feature/Customisable_AI_Chatbot_with_memory_feature.py
+++ Customisable_AI_Chatbot_with_memory_feature/Customisable_AI_Chatbot_with_memory_feature.py
@@ -1,16 +1,16 @@
-# Ai Chatbot Running Locally With Memory Feature 
-# Description= A Chatbot Running Locally, Built With Llama3.2 And Gradio. Also Have Feature To Store Conversation History
-# You Can Also Change LLM By Your Choice By Downloading Through Ollama For Changing LLM See readme.md File  
-# Created By Ojaswie Kumar Sanwariya 
+# Ai chatbot running locally with memory feature 
+# Description= a chatbot running locally, built with llama3.2 and gradio. also has a feature to store conversation history
+# You can also change LLM by your choice by downloading through Ollama for changing  see readme.md file  
+# Created by Ojaswie Kumar Sanwariya 
 
 import gradio as gr
 from openai import OpenAI
 
-client =OpenAI(base_url= "http://localhost:11434/v1", api_key="notneed")
+client=OpenAI(base_url= "http://localhost:11434/v1", api_key="notneed")
 
-def chat_with_llama(message,history):
+def chat_with_llama(message, history):
     
-    messages= [{"role": "system", "content": "you are a helpful assistant"}]
+    messages=[{"role": "system", "content": "you are a helpful assistant"}]
     
     for human, ai in history:
         messages.append({"role": "user", "content": human })
@@ -18,7 +18,7 @@ def chat_with_llama(message,history):
 
     messages.append({"role": "user", "content": message})
 
-    response= client.chat.completions.create(model="llama3.2",messages=messages,stream=True)
+    response=client.chat.completions.create(model="llama3.2",messages=messages,stream=True)
 
     result= ""
 
