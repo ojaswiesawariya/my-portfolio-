02931e10a8d1f420e381aa59938ee73ce536ac5a
ojaswiesawariya <ojaswisawariya00@gmail.com>
2026-09-07 17:27:02 +0530

    Update Customisable_AI_Chatbot_multi_model_with_memory_feature.py

--- Customisable_AI_Chatbot_multi_model_with_memory_feature/Customisable_AI_Chatbot_multi_model_with_memory_feature.py
+++ Customisable_AI_Chatbot_multi_model_with_memory_feature/Customisable_AI_Chatbot_multi_model_with_memory_feature.py
@@ -1,6 +1,7 @@
-# AI CHATBOT RUNNING LOCALLY WITH MEMORY FEATURE AND MULIT MODEL SELECTION
-# DESCRIPTION= A CHATBOT RUNING LOCALLY, BUILT WITH llama3.2, deepseek-r1:1.5b AND GRADIO. ALSO HAVE FEATURE TO STORE CONVERSATION HISTORY AND IT HAS 2 MODEL YOU CAN SELECT ANY
-#CREATED BY OJASWIE KUMAR SANWARIYA
+# ai chatbot running locally with memory feature and multi model selection
+# Description= A Chatbot Runing Locally, Built With Llama3.2, Deepseek-R1:1.5B And Gradio. Also Have Feature To Store Conversation History And It Has 2 Model You Can Select Any
+
+#Created By Ojaswie Kumar Sanwariya
 
 import gradio as gr
 from openai import OpenAI
@@ -23,7 +24,7 @@ def chat_with_multiple_model(message, history, model_choice):
             result+= chunk.choices[0].delta.content
             yield result
 
-model_dropdown= gr.Dropdown(choices=["llama3.2", "deepseek-r1:1.5b"],value="llama3.2", label= "choice you'r AI brain")
+model_dropdown= gr.Dropdown(choices=["llama3.2", "deepseek-r1:1.5b"],value="llama3.2", label= "Choose your AI brain")
 
 gr.ChatInterface(fn=chat_with_multiple_model,additional_inputs=[model_dropdown],additional_inputs_accordion=gr.Accordion(label=" Select Model", open=False),
 title= "  AI Multi chatbot" ).launch()
